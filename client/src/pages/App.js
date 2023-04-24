a837159866e7402aad880147670850f02a503c34
TreMitchell <007549757@coyote.csusb.edu>
2023-04-23 21:49:04 -0700

    added aboutus section (in public)

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import Home from './Home';
-import Signup from './Signup';
-import Login from './Login';
-import EditProfile from './EditProfile';
-import MyProfile from './MyProfile';
-import Messages from './Messages';
-import PrivateRoute from './PrivateRoute';
-import {BrowserRouter as  Router, Routes, Route} from 'react-router-dom';
-
+import React from "react";
+import Home from "./Home";
+import Signup from "./Signup";
+import Login from "./Login";
+import EditProfile from "./EditProfile";
+import MyProfile from "./MyProfile";
+import Messages from "./Messages";
+// import PrivateRoute from "./PrivateRoute";
+// import AboutUs from "./AboutUs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return(
-            <Router>
-                <Routes>
-                    <Route exact path="/"
-                    element={<Home/>}/>
-                    <Route exact path="/editprofile"
-                    element={<EditProfile />}/>
-                    <Route exact path="/myprofile"
-                    element={<MyProfile />}/>
-                    <Route path="/signup" element={<Signup />}/>
-                    <Route path="/login" element={<Login />}/>
-                    <Route path="/messages" element={<Messages />}/>
-
-                </Routes>
-            </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/editprofile" element={<EditProfile />} />
+        <Route exact path="/myprofile" element={<MyProfile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/messages" element={<Messages />} />
+        {/* <Route path="/aboutus" element={<AboutUs />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -9,92 +9,87 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql2.createConnection({
-    host:"localhost",
-    user: "root",
-    password:"CIWA@23se4550",
-    database:"smdb"
+  host: "clipit-sm-db.co8sylbqcwmx.us-east-2.rds.amazonaws.com",
+  user: "clipitadmin",
+  password: "CSE4550SMWA!",
+  database: "smdb",
 });
 
-app.get("/", (req, res)=>{
-    res.json("hello this is the backend")
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
 });
 
 app.post("/login", (req, res) => {
-    const q = "SELECT * FROM registration WHERE username = ? AND password = ?";
-    const { username, password } = req.body;
+  const q = "SELECT * FROM registration WHERE username = ? AND password = ?";
+  const { username, password } = req.body;
 
-    db.query(q, [username, password], (error, results, fields)=>{
-        if (error) throw error;
-        if (results.length > 0) {         
-            res.send("Successful login");
-        } else {
-            res.status(401).send("Invalid username or password");
-        }
-    });
+  db.query(q, [username, password], (error, results, fields) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.send("Successful login");
+    } else {
+      res.status(401).send("Invalid username or password");
+    }
+  });
 });
 
 app.post("/signup", async (req, res) => {
-    const qc = "SELECT * FROM registration WHERE username = ?";
-    const q = "INSERT INTO registration (username, password) VALUES (?,?)";
-    const { username, password } = req.body;;
+  const qc = "SELECT * FROM registration WHERE username = ?";
+  const q = "INSERT INTO registration (username, password) VALUES (?,?)";
+  const { username, password } = req.body;
 
-    db.query(qc, [username], (error, results)=>{
-        if (error) {
-            return res.status(500).send("Error"); 
-        }
-        
-        if (results.length > 0) {
-            return res.status(400).send("Username is already taken")
-        }
+  db.query(qc, [username], (error, results) => {
+    if (error) {
+      return res.status(500).send("Error");
+    }
 
-        db.query(q, [username, password], (error, results) => {
-            if (error) {
-                return res.status(500).send("Error");
-            }
-                return res.status(200).send("Successful registration");
-        });      
+    if (results.length > 0) {
+      return res.status(400).send("Username is already taken");
+    }
+
+    db.query(q, [username, password], (error, results) => {
+      if (error) {
+        return res.status(500).send("Error");
+      }
+      return res.status(200).send("Successful registration");
     });
+  });
 });
 
 app.get("/signup", (req, res) => {
-    const q = "SELECT * FROM registration"
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-app.get("/profile", (req, res)=>{
-    const q = "SELECT * FROM profile"
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
+  const q = "SELECT * FROM registration";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
-
-app.get("/posts", (req, res)=>{
-    const q = "SELECT * FROM posts"
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
+app.get("/profile", (req, res) => {
+  const q = "SELECT * FROM profile";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
-app.post("/posts", (req, res)=>{
-    const q = "INSERT INTO posts (`comment`,`postcontent`) VALUES (?)";
-    const values = [
-        req.body.comment,
-        req.body.postcontent
-    ]
-
-    db.query(q, [values], (err, data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
+app.get("/posts", (req, res) => {
+  const q = "SELECT * FROM posts";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
+app.post("/posts", (req, res) => {
+  const q = "INSERT INTO posts (`comment`,`postcontent`) VALUES (?)";
+  const values = [req.body.comment, req.body.postcontent];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8803, () => {
-    console.log("Connected to backend server");
+  console.log("Connected to backend server");
 });
